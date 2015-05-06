cded2e462117f7640cc95b661604c4e7962b8a30
Timo Dörr <timo@latecrew.de>
2015-05-06 18:17:53 +0200

    Add util class

---
+++ src/util.ts
@@ -0,0 +1,16 @@
+module PubSub {
+
+  export class Util {
+    
+    public static randomString(length?: number = 8): string
+    {
+      var text = '';
+      var allowedCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+      for(var i=0; i < length; i++)
+        text += allowedCharacters.charAt(Math.floor(Math.random() * allowedCharacters.length));
+
+      return text;
+    }
+  }
+}
