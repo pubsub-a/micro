12ce0a442f0334fc3393cd9f384c36090b0a8903
Timo Dörr <timo@latecrew.de>
2016-02-08 09:11:03 +0000

    Fix tests

--- tests/spec-validation.js
+++ tests/spec-validation.js
@@ -1,13 +1,13 @@
 (function() {
-  var pubsub = new PubSubA.MicroPubSub();
+  var pubsub = new PubSubMicro();
   var factory = function(reset) {
     if (reset === true || reset === undefined)
-      pubsub = new PubSubA.MicroPubSub();
+      pubsub = new PubSubMicro();
 
     return pubsub;
   };
 
   // if run in the context of another test suite, we make a factory known that exports our implementation
-  window.registerFactory('PubSubA.Micro', factory);
+  window.registerFactory('PubSubMicro', factory);
 
 })();
