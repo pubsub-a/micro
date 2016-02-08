ee29a048bc473b1d6e16dde9027f5215efb72443
Timo Dörr <timo@latecrew.de>
2016-02-08 09:21:29 +0000

    Add .d.ts file to dist

---
+++ dist/src/pubsub-micro.d.ts
@@ -0,0 +1,15 @@
+import { IPubSub, IChannel, IPubSubStartCallback, IPubSubStopCallback, IChannelReadyCallback } from 'pubsub-a-interface';
+import { BucketHash } from './buckethash';
+export default class PubSub implements IPubSub {
+    private subscriptionCache;
+    constructor();
+    start(callback?: IPubSubStartCallback): void;
+    stop(callback?: IPubSubStopCallback): void;
+    channel(name: string, callback: IChannelReadyCallback): IChannel;
+    static includeIn(obj: any, publish_name?: string, subscribe_name?: string): any;
+    /**
+     * Helper functions that expose some internals that are reused in sister projects
+     */
+    static BucketHash: typeof BucketHash;
+    static ChannelBlueprint: any;
+}
