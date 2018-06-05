fe8305395c06434a0b8696f255ffa37fcf20624a
Timo Dörr <timo@latecrew.de>
2018-06-05 14:59:14 +0200

    Use window.crypto for random strings if available

--- src/util.ts
+++ src/util.ts
@@ -1,9 +1,20 @@
+declare var window: any;
 export function randomString(length: number = 8): string {
     let text = '';
-    const allowedCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    // we do not use I, J, O, 0 to avoid misinterpretations by humans
+    const allowedCharacters = 'ABCDEFGHKLMNPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789';
 
-    for (var i = 0; i < length; i++)
-        text += allowedCharacters.charAt(Math.floor(Math.random() * allowedCharacters.length));
+    if (typeof window !== 'undefined' && window.crypto) {
+        let values = new Uint32Array(length);
+        window.crypto.getRandomValues(values);
+        for (let i = 0; i < length; i++) {
+            text += allowedCharacters.charAt(values[i] % length);
+        }
+    } else {
+        for (let i = 0; i < length; i++) {
+            text += allowedCharacters.charAt(Math.floor(Math.random() * allowedCharacters.length));
+        }
+    }
 
     return text;
 }
