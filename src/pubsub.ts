672893a047f7bd9aeb193a3c92ac79808a9d53d2
Timo Dörr <timo@latecrew.de>
2018-06-27 10:03:49 +0200

    Export addValidation

--- src/pubsub.ts
+++ src/pubsub.ts
@@ -1,7 +1,7 @@
 export { BucketHash } from "./buckethash";
 export { PubSubMicroUnvalidated, PubSubMicroValidated as PubSubMicro } from "./pubsub-micro";
 export { SubscriptionTokenImpl as SubscriptionToken } from "./subscription-token";
-export { ChannelProxy, PubSubValidationProxy } from "./validation-wrapper";
+export { addValidation } from "./validation-wrapper";
 export { randomString } from "./util"
 export * from "./string-validation";
 export { invokeIfDefined, safeDispose } from "./helper";
