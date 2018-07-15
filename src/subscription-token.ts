5f3239bb6a16c4bbe534804f6e061663a9449a5f
Timo Dörr <timo@latecrew.de>
2018-07-15 10:13:15 +0200

    Remove subscription count and bump deps

--- src/subscription-token.ts
+++ src/subscription-token.ts
@@ -1,24 +1,22 @@
 import { SubscriptionToken as ISubscriptionToken } from '@dynalon/pubsub-a-interfaces';
 
 export interface IDisposeFunction {
-    (): Promise<number>;
+    (): Promise<any>;
 }
 
 export class SubscriptionToken implements ISubscriptionToken {
 
     public isDisposed: boolean = false;
-    public count: number;
 
     private disposeFn: IDisposeFunction;
 
     constructor(onDispose: IDisposeFunction, count?: number) {
         this.disposeFn = onDispose;
-        this.count = count ? count : 0;
     }
 
-    dispose(): Promise<number> {
+    dispose(): Promise<void> {
         if (this.isDisposed) {
-            return Promise.resolve(0);
+            return Promise.resolve();
         }
 
         this.isDisposed = true;
