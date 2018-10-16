bd9c8174adbdbaa6c179aead104669c249a2cbda
Timo Dörr <timo@latecrew.de>
2018-10-16 16:19:14 +0200

    Remove number 1 from randomString()

--- src/util.ts
+++ src/util.ts
@@ -1,8 +1,8 @@
 declare var window: any;
 export function randomString(length: number = 8): string {
     let text = '';
-    // we do not use I, J, O, 0, l to avoid misinterpretations by humans
-    const allowedCharacters = 'ABCDEFGHKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz123456789';
+    // we do not use I, J, O, 0, l, 1 to avoid misinterpretations by humans
+    const allowedCharacters = 'ABCDEFGHKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz23456789';
 
     if (typeof window !== 'undefined' && window.crypto) {
         let values = new Uint32Array(length);
